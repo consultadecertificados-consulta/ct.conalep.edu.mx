1a281ae7e53edfebdfb9833f8524cb7e309500d1
consultadecertificados-consulta <consultadecertificados@gmail.com>
2026-07-31 21:08:54 -0600

    Add multiple folios with student details

--- folios.js
+++ folios.js
@@ -14,6 +14,53 @@ const baseDatos = {
         estatus: "Registrado en el MEC",
         folio: "CBG3760391305",
         fechaTimbrado: "2018-07-19 10:21:35"
-    }
-    // Agrega más folios aquí siguiendo el mismo formato
+    },
+    "CBG3760539125146": {
+        nombre: "BENITO CAMELA",
+        matricula: "1755M425555",
+        institucion: "Colegio Nacional de Educación Profesional Técnica",
+        plantel: "023 Prof. Joaquin Humberto Gonzalez Loza",
+        cct: "05DPT0003Z",
+        carrera: "Mantenimiento de sistemas automáticos",
+        trayecto: "Automatización y control de procesos industriales",
+        promedio: "8.5 (Ocho punto cinco)",
+        creditos: "378 créditos obtenidos de un total de 378",
+        periodo: "Del 1 de agosto de 2015 al 15 de julio de 2018",
+        tipoDocumento: "Certificado de Terminación de Estudios o Certificado de Formación Profesional",
+        estatus: "Registrado en el MEC",
+        folio: "CBG3760391305",
+        fechaTimbrado: "2018-07-19 10:21:35"
+            },
+    "CBG37603913052": {
+        nombre: "ADRIAN ACOSTA MATA",
+        matricula: "1755M425555",
+        institucion: "Colegio Nacional de Educación Profesional Técnica",
+        plantel: "023 Prof. Joaquin Humberto Gonzalez Loza",
+        cct: "05DPT0003Z",
+        carrera: "Mantenimiento de sistemas automáticos",
+        trayecto: "Automatización y control de procesos industriales",
+        promedio: "8.5 (Ocho punto cinco)",
+        creditos: "378 créditos obtenidos de un total de 378",
+        periodo: "Del 1 de agosto de 2015 al 15 de julio de 2018",
+        tipoDocumento: "Certificado de Terminación de Estudios o Certificado de Formación Profesional",
+        estatus: "Registrado en el MEC",
+        folio: "CBG3760391305",
+        fechaTimbrado: "2018-07-19 10:21:35"
+},
+    "CBG37603913053": {
+        nombre: "ADRIAN ACOSTA MATA",
+        matricula: "1755M425555",
+        institucion: "Colegio Nacional de Educación Profesional Técnica",
+        plantel: "023 Prof. Joaquin Humberto Gonzalez Loza",
+        cct: "05DPT0003Z",
+        carrera: "Mantenimiento de sistemas automáticos",
+        trayecto: "Automatización y control de procesos industriales",
+        promedio: "8.5 (Ocho punto cinco)",
+        creditos: "378 créditos obtenidos de un total de 378",
+        periodo: "Del 1 de agosto de 2015 al 15 de julio de 2018",
+        tipoDocumento: "Certificado de Terminación de Estudios o Certificado de Formación Profesional",
+        estatus: "Registrado en el MEC",
+        folio: "CBG3760391305",
+        fechaTimbrado: "2018-07-19 10:21:35"
+}
 };
