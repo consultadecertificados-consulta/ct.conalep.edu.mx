c8afafee2c86427fa477c92d2a65c19e582827a6
consultadecertificados-consulta <consultadecertificados@gmail.com>
2026-08-08 15:40:47 -0600

    Update folios.js

--- folios.js
+++ folios.js
@@ -1,19 +1,19 @@
 const baseDatos = {
-    "CBG3760391305": {
-        nombre: "ADRIAN ACOSTA MATA",
-        matricula: "1755M425555",
+    "CBG3760391205147": {
+        nombre: "JUANA SANCHEZ AGUILERA.",
+        matricula: "1702R950249",
         institucion: "Colegio Nacional de Educación Profesional Técnica",
-        plantel: "023 Prof. Joaquin Humberto Gonzalez Loza",
-        cct: "05DPT0003Z",
-        carrera: "Mantenimiento de sistemas automáticos",
-        trayecto: "Automatización y control de procesos industriales",
-        promedio: "8.5 (Ocho punto cinco)",
-        creditos: "378 créditos obtenidos de un total de 378",
-        periodo: "Del 1 de agosto de 2015 al 15 de julio de 2018",
+        plantel: "Plantel Celaya 028",
+        cct: "1702R950249",
+        carrera: "Administración",
+        trayecto: "trayecto técnico de especialización regional o de cobranza y un trayecto propedéutico paracontinuar a la universidad",
+        promedio: "8.6 (Ocho punto seis)",
+        creditos: "360 créditos obtenidos de un total de 360",
+        periodo: "Del 13 de agosto de 2004 al 18 de junio de 2006",
         tipoDocumento: "Certificado de Terminación de Estudios o Certificado de Formación Profesional",
         estatus: "Registrado en el MEC",
-        folio: "CBG3760391305",
-        fechaTimbrado: "2018-07-19 10:21:35"
+        folio: "CBG3760391205147",
+        fechaTimbrado: "2006-06-18 16:20:11"
     
     },
     
